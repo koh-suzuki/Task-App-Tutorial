46ac38c7d6a90856d06f51d56fb7bae1bd3f6840
koh-suzuki <k.s_coward_0629@icloud.com>
2019-08-28 07:33:46 +0000

    task_function_implementation Finish

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  require "uri"
 
   def full_title(page_name = "")
     base_title = "Task App"
@@ -7,5 +8,14 @@ module ApplicationHelper
     else
       page_name + " | " + base_title
     end
+  end
+  
+  def text_url_to_link text
+    URI.extract(text, ['http']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+      text.gsub!(url, sub_text)
+    end
+    return text
   end
 end
