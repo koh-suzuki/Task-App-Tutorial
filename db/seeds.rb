7c93963c2bd5adbe053e87113f0008c636976cda
koh-suzuki <k.s_coward_0629@icloud.com>
2019-08-29 08:26:43 +0000

    D6

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,14 @@ User.create!( name: "Sample User",
                password: password,
                password_confirmation: password)
 end
+
+
+Task.create!( task_name: "task1",
+              task_description: "タスクの作成")
+
+50.times do |n|
+  task_name  = Faker::Name.task_name
+  task_description = "タスクの作成(#{n+1})"
+  Task.create!(task_name: task_name,
+               task_description: task_description)
+end
